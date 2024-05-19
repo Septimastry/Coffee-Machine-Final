9c4256ad52a27326352dad977352e43cc07abd4d
Jason Desktop <chishuntse.work@gmail.com>
2024-05-19 14:29:30 +0100

    Finished and bug fixed

--- main.py
+++ main.py
@@ -31,11 +31,11 @@ stock = {
 }
 
 # Variables
+money = 0.0
 P = 0.01
 N = 0.05
 D = 0.1
 Q = 0.25
-total = float(0)
 start = True
 
 
@@ -60,14 +60,20 @@ def deduct(name):
 
 
 # Take coins
-def take_coins(q, d, n, p, name):
-    total = q*Q + d*D + n*N +p*P
+def take_coins(q, d, n, p, name, m):
+    total = float(q)*Q + float(d)*D + float(n)*N + float(p)*P
     if total > MENU[name]["cost"]:
-
+        change = total - MENU[name]["cost"]
+        profit = MENU[name]["cost"]
+        m += profit
+        return change, m, True
+    else:
+        print("Not enough money, coins refunded.")
+        return 0, m, False
 
 
 # Make coffee
-def make_coffee(name):
+def make_coffee(name, mon):
     possible = check_possible(name)
     if possible:
         print("Please insert coin.")
@@ -75,19 +81,23 @@ def make_coffee(name):
         dime = input("How many dimes?")
         nickel = input("How many nickels?")
         penny = input("How many pennies?")
-        take_coins(quarter, dime, nickel, penny, name)
-        deduct(name)
+        change, mon, proceed = take_coins(quarter, dime, nickel, penny, name, mon)
+        if proceed:
+            print(f"Here is ${round(change, 2)} change.")
+            print(f"Here is your {name} ☕ enjoy!")
+            deduct(name)
+        return mon
 
 
 while start:
     user = str(input("What do you want?"))
     user = user.lower()
-    if user != "off" or user != "report" or user != "latte" or user != "espresso" or user != "cappuccino":
+    if user != "off" and user != "report" and user != "latte" and user != "espresso" and user != "cappuccino":
         print("Invalid input")
     elif user == "off":
         start = False
     elif user == "report":
-        print(f"Water: {stock["water"]}\nMilk: {stock["milk"]}\nCoffee: {stock["milk"]}")
+        print(f"Water: {stock["water"]}\nMilk: {stock["milk"]}\nCoffee: {stock["milk"]}\nProfit: {money}")
         # Print current ingredient left
     else:
-        make_coffee(user)
+        money = make_coffee(user, money)
